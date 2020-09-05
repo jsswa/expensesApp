9e3f78a4a7f55698429d060ad6906e344e8a4a6f
Vayango <vayango@hotmail.fr>
2020-09-05 20:09:40 +0200

    Updates

---
+++ precache-manifest.be6b280d9794604444f33b89ef07a5ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "872d1143f477c5a95c7f3d60e8c0870c",
+    "url": "/expensesApp/index.html"
+  },
+  {
+    "revision": "63079ecb68353bf6de00",
+    "url": "/expensesApp/static/css/main.add952c5.chunk.css"
+  },
+  {
+    "revision": "1d1e6de6f5baa6b08d06",
+    "url": "/expensesApp/static/js/2.04d64768.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/expensesApp/static/js/2.04d64768.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63079ecb68353bf6de00",
+    "url": "/expensesApp/static/js/main.c3533fd6.chunk.js"
+  },
+  {
+    "revision": "160fc605fa681ad0f3f4",
+    "url": "/expensesApp/static/js/runtime-main.9fbfd1da.js"
+  }
+]);
